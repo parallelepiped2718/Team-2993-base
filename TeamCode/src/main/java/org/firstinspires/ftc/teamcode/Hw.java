b74afc579726019942225d1cf06ec8eb525c21da
parallelepiped2718 <goggles7337@gmail.com>
2023-02-04 17:21:08 -0500

    Reworked to have a separate hardware class to deal with the hardware stuff, and one OpMode class to handle user input

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hw.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import java.lang.Math;
+
+
+public class Hw {
+
+  public Hw (HardwareMap hwMap)
+  {
+    //init stuff here
+  }
+
+  public void setFrontLeft () {} //todo
+  public void setFrontRight () {} //todo
+  public void setBackLeft () {} //todo
+  public void setBackRight () {} //todo
+  
+}
